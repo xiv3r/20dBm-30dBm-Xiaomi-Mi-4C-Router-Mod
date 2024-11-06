6507569c551231f57e405ea15d42a594ef892f0a
xiv3r <117867334+xiv3r@users.noreply.github.com>
2024-11-06 16:57:27 +0800

    Update mtd2-mod.sh

--- mtd2-mod.sh
+++ mtd2-mod.sh
@@ -2,9 +2,7 @@
 
 insmod mtd-rw i_want_a_brick=1
 echo "insmod done...!"
-dd if=/dev/mtd2 of=/tmp/mtd2.bin
-echo "Copying mtd2...!"
-printf '\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff' |dd of=/tmp/mtd2.bin bs=1 seek=160 count=14 conv=notrunc
+dd if=/dev/mtd2 of=/tmp/mtd2.bin && printf '\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff' |dd of=/tmp/mtd2.bin bs=1 seek=160 count=14 conv=notrunc
 echo "Done modding mtd2...
 echo "writing mtd2 to factory..."
 mtd -r write /tmp/mtd2.bin factory
